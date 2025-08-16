d6ca600f92df2a728520a4b7bb996f09fecad705
Venkata Somanath Meda <154015759+Somanath-03@users.noreply.github.com>
2025-08-16 20:33:23 +0530

    Update route.ts

--- src/app/api/edit-post/route.ts
+++ src/app/api/edit-post/route.ts
@@ -35,7 +35,7 @@ export async function PUT(request: Request) {
       return NextResponse.json({ error: uploadError.message }, { status: 500 });
     }
 
-    fileUrl = `https://ujbncvgdlulldcjkwcbk.supabase.co/storage/v1/object/public/ImgAndVid/${uploadData.path}`;
+    fileUrl = publicData.publicUrl;
   }
 
   const { data, error } = await supabase
